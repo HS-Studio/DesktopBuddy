26c4f300fbd3599fd9098598c8d63ef65967f746
Heiko <heiko-schulz@live.de>
2026-04-16 23:29:12 +0200

    added/updated shape

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ void setup()
     Serial.println("Starting...");
     Serial.println();
 
-    //pinMode(BUTTON_PIN, INPUT_PULLUP);
+    // pinMode(BUTTON_PIN, INPUT_PULLUP);
     pinMode(BLK_PIN, OUTPUT);
     pinMode(JOY_X_PIN, INPUT);
     pinMode(JOY_Y_PIN, INPUT);
@@ -47,7 +47,7 @@ void setup()
     eyePair.target = default_state;
 
     blinkbefore = eyePair.target.blink;
-    sizebefore = eyePair.target.size;
+    scalebefore = eyePair.target.scale;
 
     eyePair.convergence = 0.5f;
 
@@ -74,16 +74,12 @@ void loop()
     joy.y = ((float)analogRead(JOY_Y_PIN) - 1875.0f) / 1875.0f;
 
     joy.x = constrain(joy.x, -1, 1);
-    joy.y = constrain(joy.y, -1, 1) * -1;
+    joy.y = constrain(joy.y, -1, 1);
 
-    // Ziel setzen
-    eyePair.target.gaze.x = joy.x; // Später automatisch / random / Emotion
-    eyePair.target.gaze.y = joy.y;
-
-    blink(eyePair.current, eyePair.target, 0.95f);
+    blink(eyePair.current, eyePair.target, 0.98f);
 
     // smooth interpolation
-    updateEyeState(eyePair.current, eyePair.target, 0.2f);
+    updateEyeState(eyePair.current, eyePair.target, 0.20f);
 
     // Augen ableiten
     eyeL = eyePair.current;
@@ -93,38 +89,33 @@ void loop()
 
     // Konvergenz
     eyeL.pos.x += eyePair.convergence;
-    eyeR.pos.x -= eyePair.convergence;
+    eyeR.pos.x += eyePair.convergence;
 
-    // Mikrobewegung
-    float t = millis() * 0.002f;
-    eyePair.target.gaze.x += sin(millis() * 0.002f) * 0.20f;
-    eyePair.target.gaze.y += sin(millis() * 0.004f) * 0.19f;
+    drawFace(eyePair, eyeL, eyeR, 24, 100);
 
-    drawFace(eyePair, eyeL, eyeR, 20, 80);
+    // eyePair.current.brightnes = eyePair.current.scale.y*255;
 
-    //eyePair.current.brightnes = eyePair.current.size.y*255;
-
-    if (digitalRead(BUTTON_PIN) == LOW)
+    // Ziel setzen
+    if (joy.x > 0)
     {
-        if (buttonUp)
-        {
-            Serial.println("BUTTON!!");
-            uint8_t r = random(0,255);
-            uint8_t g = random(0,255);
-            uint8_t b = random(0,255);
-
-            eyePair.target.color = lgfx::v1::rgb888_t({r, g, b});
-            buttonUp = false;
-            // delay(100);
-        }
+        eyePair.target.angry = joy.x;
+        eyePair.target.happy = 0.0f;
     }
     else
     {
-        buttonUp = true;
+        eyePair.target.happy = -joy.x;
+        eyePair.target.angry = 0.0f;
     }
 
-    //Serial.printf(">joy.x: %f\n>joy.y: %f\n", joy.x, joy.y);
-    //Serial.printf(">blink: %f\n", eyePair.current.blink);
+    eyePair.target.gaze.x = joy.y; // Später automatisch / random / Emotion
+    // eyePair.target.gaze.y = joy.x;
+
+    if (eyePair.target.angry >= 0.5)
+        eyePair.target.color = lgfx::v1::rgb888_t({uint8_t(eyePair.target.angry * 255), 0, 0});
+    else
+        eyePair.target.color = default_state.color;
+    // Serial.printf(">joy.x: %f\n>joy.y: %f\n", joy.x, joy.y);
+    // Serial.printf(">blink: %f\n", eyePair.current.blink);
 
     if (showFps)
         drawFPS();
@@ -161,17 +152,6 @@ void buildShape(BezierLine *shape, int count, int steps, std::vector<Point> &pts
     for (int i = 0; i < count; i++)
     {
         sampleBezier(shape[i], pts, steps);
-    }
-}
-
-void normalizeToScreen(std::vector<Point> &pts, uint16_t w, uint16_t h, int16_t ox, int16_t oy)
-{
-    for (auto &p : pts)
-    {
-        p.x *= (w - 1);
-        p.y *= (h - 1);
-        p.x += (MAX_W / 2 - w / 2) + ox;
-        p.y += (MAX_H / 2 - h / 2) + oy;
     }
 }
 
@@ -236,7 +216,7 @@ void drawEye(LGFX_Sprite &eyeSpr, EyeState &e, uint16_t screen_x, uint16_t scree
     int16_t y = e.pos.y * MAX_H * 0.25;
 
     buildShape(baseShape, 4, bezierRes, pts);
-    normalizeToScreen(pts, e.size.x * MAX_W, e.size.y * MAX_H, x, y); // eyeL pos
+    normalizeToScreen(pts, e.scale.x * MAX_W, e.scale.y * MAX_H, x, y); // eyeL pos
     fillPolygon(pts, maskSprite, TFT_WHITE);
 
     radialGradient.pushSprite(&eyeSpr, gX + x, gY + y); // pupill
@@ -246,34 +226,37 @@ void drawEye(LGFX_Sprite &eyeSpr, EyeState &e, uint16_t screen_x, uint16_t scree
 }
 */
 
-void drawEye(LGFX_Sprite &eyeSpr, EyeState &e, EyeRenderCache& cache, uint16_t screen_x, uint16_t screen_y)
+void drawEye(LGFX_Sprite &eyeSpr, EyeState &e, EyeRenderCache &cache, uint16_t screen_x, uint16_t screen_y, bool mirror)
 {
     eyeSpr.fillSprite(pupilGradient[3]);
     maskSprite.fillSprite(TFT_BLACK);
 
-    updateShapeCache(cache, e);
-    
-    int16_t gX = round(e.gaze.x * MAX_X);
-    int16_t gY = round(e.gaze.y * MAX_Y);
+    updateShapeCache(cache, e, mirror);
+
+    int16_t gX = (e.gaze.x * MAX_X);
+    int16_t gY = (e.gaze.y * MAX_Y);
 
     int16_t x = e.pos.x * (MAX_W * 0.25);
     int16_t y = e.pos.y * (MAX_H * 0.25);
 
+    if (mirror)
+        gX -= (eyePair.convergence * MAX_W) * 0.5f;
+
     fillPolygonET(cache, maskSprite, TFT_WHITE);
-    //fillPolygon(cache.pts, maskSprite, TFT_WHITE);
+    // fillPolygon(cache.pts, maskSprite, TFT_WHITE);
 
     radialGradient.pushSprite(&eyeSpr, gX + x, gY + y); // pupill
     maskSprite.pushSprite(&eyeSpr, 0, 0, TFT_WHITE);
-    
-/*  //visualize points
-    int n = cache.pts.size(); 
 
-    for(int i = 0; i < n; i++)
-    {
-        Point p = cache.pts[i];
-        eyeSpr.drawRect(p.x, p.y, 2, 2, TFT_WHITE);
-    }
- */
+    /*  //visualize points
+        int n = cache.pts.size();
+
+        for(int i = 0; i < n; i++)
+        {
+            Point p = cache.pts[i];
+            eyeSpr.drawRect(p.x, p.y, 2, 2, TFT_WHITE);
+        }
+     */
     eyeSpr.pushSprite(screen_x, screen_y);
 }
 
@@ -282,13 +265,13 @@ void drawFace(EyePair &pair, EyeState &eL, EyeState &eR, int screen_x, int scree
     int16_t x = screen_x + eL.gaze.x * MAX_X;
     int16_t y = screen_y + eR.gaze.y * MAX_Y;
 
-    //drawEyeClipped(eyeLSprite, eL, x, y);
-    //drawEyeClipped(eyeRSprite, eR, x + MAX_W, y);
-    //drawEye(eyeLSprite, eL, x, y);
-    //drawEye(eyeRSprite, eR, x + MAX_W, y);
-    
+    // drawEyeClipped(eyeLSprite, eL, x, y);
+    // drawEyeClipped(eyeRSprite, eR, x + MAX_W, y);
+    // drawEye(eyeLSprite, eL, x, y);
+    // drawEye(eyeRSprite, eR, x + MAX_W, y);
+
     drawEye(eyeLSprite, eL, cacheL, x, y);
-    drawEye(eyeRSprite, eR, cacheR, x + MAX_W, y);
+    drawEye(eyeRSprite, eR, cacheR, x + MAX_W, y, true);
     ledcWrite(PWM_CHANNEL, pair.current.brightnes);
 }
 
@@ -297,7 +280,15 @@ float lerp(float a, float b, float t)
     return a + (b - a) * t;
 }
 
-Point lerp(Point a, Point b, float t)
+Point lerp(Point &a, Point &b, float t)
+{
+    Point c;
+    c.x = lerp(a.x, b.x, t);
+    c.y = lerp(a.y, b.y, t);
+    return c;
+}
+
+Point lerp(const Point &a, const Point &b, float t)
 {
     Point c;
     c.x = lerp(a.x, b.x, t);
@@ -308,6 +299,15 @@ Point lerp(Point a, Point b, float t)
 uint8_t lerp(uint8_t a, uint8_t b, float t)
 {
     return a + (b - a) * t;
+}
+
+BezierLine lerp(const BezierLine &a, const BezierLine &b, float t)
+{
+    return {
+        lerp(a.ps, b.ps, t),
+        lerp(a.pe, b.pe, t),
+        lerp(a.c1, b.c1, t),
+        lerp(a.c2, b.c2, t)};
 }
 
 lgfx::rgb888_t lerpColor(const lgfx::rgb888_t &a, const lgfx::rgb888_t &b, float t)
@@ -361,12 +361,14 @@ void updateEyeState(EyeState &eye, EyeState &target, float speed)
 {
     eye.gaze = lerp(eye.gaze, target.gaze, speed);
     eye.pos = lerp(eye.pos, target.pos, speed);
-    eye.size = lerp(eye.size, target.size, speed);
+    eye.scale = lerp(eye.scale, target.scale, speed);
     eye.pupilSize = lerp(eye.pupilSize, target.pupilSize, speed);
     eye.blink = lerp(eye.blink, target.blink, speed);
     eye.squash = lerp(eye.squash, target.squash, speed);
     eye.upperLid = lerp(eye.upperLid, target.upperLid, speed);
     eye.lowerLid = lerp(eye.lowerLid, target.lowerLid, speed);
+    eye.angry = lerp(eye.angry, target.angry, speed);
+    eye.happy = lerp(eye.happy, target.happy, speed);
     eye.brightnes = lerp(eye.brightnes, target.brightnes, speed);
 
     if (updateColor(eye.color, target.color, speed))
@@ -377,48 +379,45 @@ void updateEyeState(EyeState &eye, EyeState &target, float speed)
     }
 }
 
-bool hasChanged(const Point& a, const Point& b, float eps)
+bool hasChanged(const Point &a, const Point &b, float eps)
 {
     return (abs(a.x - b.x) > eps) || (abs(a.y - b.y) > eps);
 }
 
-void updateShapeCache(EyeRenderCache& cache, EyeState& e)
+void updateShapeCache(EyeRenderCache &cache, EyeState &e, bool mirror)
 {
-    if (hasChanged(e.size, cache.lastState.size) ||
+    if (hasChanged(e.scale, cache.lastState.scale) ||
         hasChanged(e.pos, cache.lastState.pos) ||
         abs(e.blink - cache.lastState.blink) > 0.001f ||
+        abs(e.angry - cache.lastState.angry) > 0.001f ||
+        abs(e.happy - cache.lastState.happy) > 0.001f ||
         cache.dirty)
     {
-        cache.pts.clear();
+        // bezier deform
+        BezierLine tempShape1[4];
+        BezierLine tempShape2[4];
+        BezierLine tempShape3[4];
+
+        morphShape(tempShape1, baseShape, angryShape, e.angry);
+        morphShape(tempShape2, tempShape1, happyShape, e.happy);
+        morphShape(tempShape3, tempShape2, blinkShape, e.blink);
+
+        deformShape(tempShape3, e, mirror);
 
         // Shape bauen
-        buildShape(baseShape, 4, bezierRes, cache.pts);
-        
-        applyLids(cache.pts, e);
-
-        int16_t x = e.pos.x * MAX_W * 0.25f;
-        int16_t y = e.pos.y * MAX_H * 0.25f;
-
-        // Transformieren
-        normalizeToScreen(
-            cache.pts,
-            e.size.x * MAX_W,
-            e.size.y * MAX_H,
-            x,
-            y
-        );
+        buildShape(tempShape3, 4, bezierRes, cache.pts);
 
         // Edge Table bauen
         buildEdgeTable(cache);
 
-        cache.lastState.size = e.size;
-        cache.lastState.pos  = e.pos;
+        cache.lastState.scale = e.scale;
+        cache.lastState.pos = e.pos;
         cache.lastState.blink = e.blink;
         cache.dirty = false;
     }
 }
 
-void buildEdgeTable(EyeRenderCache& cache)
+void buildEdgeTable(EyeRenderCache &cache)
 {
     cache.ET.clear();
     cache.ET.resize(MAX_H);
@@ -428,23 +427,27 @@ void buildEdgeTable(EyeRenderCache& cache)
 
     int n = cache.pts.size();
 
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         Point p1 = cache.pts[i];
         Point p2 = cache.pts[(i + 1) % n];
 
-        if ((int)p1.y == (int)p2.y) continue; // horizontale ignorieren
+        if ((int)p1.y == (int)p2.y)
+            continue; // horizontale ignorieren
 
-        if(p1.y > p2.y) std::swap(p1, p2);
+        if (p1.y > p2.y)
+            std::swap(p1, p2);
 
         int yMin = (int)p1.y;
         int yMax = (int)p2.y;
 
-        //int yMin = (int)round(p1.y); // langsamer aber genauer
-        //int yMax = (int)round(p2.y);
+        // int yMin = (int)round(p1.y); // langsamer aber genauer
+        // int yMax = (int)round(p2.y);
 
-        if (yMin < 0) yMin = 0;
-        if (yMax >= MAX_H) yMax = MAX_H - 1;
+        if (yMin < 0)
+            yMin = 0;
+        if (yMax >= MAX_H)
+            yMax = MAX_H - 1;
 
         Edge e;
         e.yMax = yMax;
@@ -459,58 +462,108 @@ void buildEdgeTable(EyeRenderCache& cache)
     }
 }
 
-void fillPolygonET(EyeRenderCache& cache, LGFX_Sprite& spr, uint16_t color)
+void fillPolygonET(EyeRenderCache &cache, LGFX_Sprite &spr, uint16_t color)
 {
     cache.AET.clear();
 
-    for(int y = cache.minY; y < cache.maxY; y++)
+    for (int y = cache.minY; y < cache.maxY; y++)
     {
         // hinzufügen
-        for(auto& e : cache.ET[y])
+        for (auto &e : cache.ET[y])
             cache.AET.push_back(e);
 
         // entfernen
         cache.AET.erase(
             std::remove_if(cache.AET.begin(), cache.AET.end(),
-                [y](const Edge& e){ return y >= e.yMax; }),
-            cache.AET.end()
-        );
+                           [y](const Edge &e)
+                           { return y >= e.yMax; }),
+            cache.AET.end());
 
         // sortieren
         std::sort(cache.AET.begin(), cache.AET.end(),
-            [](const Edge& a, const Edge& b){ return a.x < b.x; });
+                  [](const Edge &a, const Edge &b)
+                  { return a.x < b.x; });
 
         // zeichnen
-        for(int i = 0; i + 1 < cache.AET.size(); i += 2)
+        for (int i = 0; i + 1 < cache.AET.size(); i += 2)
         {
             int x0 = (int)cache.AET[i].x;
-            int x1 = (int)cache.AET[i+1].x;
+            int x1 = (int)cache.AET[i + 1].x;
 
             if (x1 > x0)
                 spr.drawFastHLine(x0, y, x1 - x0, color);
         }
 
         // update
-        for(auto& e : cache.AET)
+        for (auto &e : cache.AET)
             e.x += e.invSlope;
     }
 }
 
-void applyLids(std::vector<Point>& pts, const EyeState& e)
+void deformShape(BezierLine *shape, const EyeState &e, bool mirror)
 {
-    float blink = e.blink;
+    float scaleX = e.scale.x;
+    float scaleY = e.scale.y;
 
-    float upperLimit = 1.0f - (blink * 0.25f);
-    float lowerLimit = (blink * 0.75f);
+    float posX = e.pos.x;
+    float posY = e.pos.y;
 
-    for(auto& p : pts)
+    // Basis-Eckpunkte (0..1)
+    Point topLeft = shape[0].ps;
+    Point topRight = shape[0].pe;
+    Point bottomRight = shape[2].ps;
+    Point bottomLeft = shape[2].pe;
+
+    // SCALE + POSITION (JETZT HIER!)
+    auto transform = [&](Point &p)
     {
-        float t = (p.y - lowerLimit) / (upperLimit - lowerLimit);
-        t = constrain(t, 0.0f, 1.0f);
-        p.y = lowerLimit + t * (upperLimit - lowerLimit);
+        // scale
+        p.x *= scaleX * (MAX_W - 2);
+        p.y *= scaleY * (MAX_H - 2);
 
-        if(p.y > upperLimit) p.y = upperLimit;
-        if(p.y < lowerLimit) p.y = lowerLimit;
+        // center + offset
+        p.x += (MAX_W / 2 - (scaleX * MAX_W) / 2) + posX * MAX_W * 0.25f;
+        p.y += (MAX_H / 2 - (scaleY * MAX_H) / 2) + posY * MAX_H * 0.25f;
+
+        // Y flip
+        p.y = (MAX_H - 2) - p.y;
+        if (mirror)
+        {
+            p.x = (MAX_W - 2) - p.x;
+        }
+    };
+
+    transform(topLeft);
+    transform(topRight);
+    transform(bottomRight);
+    transform(bottomLeft);
+
+    // Segmente setzen
+    shape[0].ps = topLeft;
+    shape[0].pe = topRight;
+
+    shape[1].ps = topRight;
+    shape[1].pe = bottomRight;
+
+    shape[2].ps = bottomRight;
+    shape[2].pe = bottomLeft;
+
+    shape[3].ps = bottomLeft;
+    shape[3].pe = topLeft;
+
+    // Kontrollpunkte
+    for (int i = 0; i < 4; i++)
+    {
+        transform(shape[i].c1);
+        transform(shape[i].c2);
+    }
+}
+
+void morphShape(BezierLine *out, const BezierLine *base, const BezierLine *target, float t)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        out[i] = lerp(base[i], target[i], t);
     }
 }
 
@@ -521,17 +574,14 @@ void blink(EyeState &e, EyeState &target, float blink)
     if (currentMillis - animMillis >= random(4000, 8000))
     {
         blinkbefore = target.blink;
-        sizebefore = target.size;
         target.blink = blink;
-        //target.size.y = 0.02;
-        target.size.x = target.size.x * 1.25;
         animMillis = currentMillis;
     }
     if (blink - e.blink < 0.15)
     {
         target.blink = blinkbefore;
-        target.size = sizebefore;
     }
+    Serial.println(e.blink);
 }
 
 void drawFPS()
--- src/main.h
+++ src/main.h
@@ -6,15 +6,15 @@
 #include <vector>
 
 #include <Arduino.h>
-//#include "LGFX_SPI_ILI9341.h"     // My Large Display
-#include "LGFX_SPI_ST7789.h"        // My Small Display
+// #include "LGFX_SPI_ILI9341.h"     // My Large Display
+#include "LGFX_SPI_ST7789.h" // My Small Display
 
 #define BUTTON_PIN 0
 
 #define BLK_PIN 33
 
-#define JOY_X_PIN  34 // ESP32 pin GPIO39 (ADC3) connected to VRX pin
-#define JOY_Y_PIN  35 // ESP32 pin GPIO36 (ADC0) connected to VRY pin
+#define JOY_X_PIN 34 // ESP32 pin GPIO39 (ADC3) connected to VRX pin
+#define JOY_Y_PIN 35 // ESP32 pin GPIO36 (ADC0) connected to VRY pin
 
 #define MAX_W 96
 #define MAX_H 96
@@ -22,24 +22,32 @@
 #define MAX_X 16
 #define MAX_Y 16
 
-bool buttonUp = false;
-
 struct Point
 {
     float x;
     float y;
 };
 
+struct BezierLine
+{
+    Point ps; // point start
+    Point pe; // point end
+    Point c1; // controll 1
+    Point c2; // controll 2
+};
+
 struct EyeState
 {
     Point gaze;
     Point pos;
-    Point size;
+    Point scale;
     float pupilSize;
     float blink;
     float squash;
     float upperLid;
     float lowerLid;
+    float angry;
+    float happy;
     lgfx::rgb888_t color;
     uint8_t brightnes;
 };
@@ -50,14 +58,6 @@ struct EyePair
     EyeState target;
 
     float convergence;
-};
-
-struct BezierLine
-{
-    Point ps;
-    Point pe;
-    Point c1;
-    Point c2;
 };
 
 struct Edge
@@ -81,85 +81,169 @@ struct EyeRenderCache
     bool dirty = true;
 };
 
-EyeRenderCache cacheL;
-EyeRenderCache cacheR;
-
 static BezierLine baseShape[4] =
     {
         // top
         {
-            {0.1, 0.9}, // p1
-            {0.9, 0.9}, // p2
-            {0.2, 1.0}, // c1
-            {0.8, 1.0}, // c2
+            {0.10f, 0.90f}, // p1
+            {0.90f, 0.90f}, // p2
+            {0.20f, 1.00f}, // c1
+            {0.80f, 1.00f}, // c2
         },
         // right
         {
-            {0.9, 0.9}, // p1
-            {0.9, 0.1}, // p2
-            {1.0, 0.8}, // c1
-            {1.0, 0.2}, // c2
+            {0.90f, 0.90f}, // p1
+            {0.90f, 0.10f}, // p2
+            {1.00f, 0.80f}, // c1
+            {1.00f, 0.20f}, // c2
         },
         // bottom
         {
-            {0.9, 0.1}, // p1
-            {0.1, 0.1}, // p2
-            {0.8, 0.0}, // c1
-            {0.2, 0.0}, // c2
+            {0.90f, 0.10f}, // p1
+            {0.10f, 0.10f}, // p2
+            {0.80f, 0.00f}, // c1
+            {0.20f, 0.00f}, // c2
         },
         // left
         {
-            {0.1, 0.1}, // p1
-            {0.1, 0.9}, // p2
-            {0.0, 0.2}, // c1
-            {0.0, 0.8}, // c2
+            {0.10f, 0.10f}, // p1
+            {0.10f, 0.90f}, // p2
+            {0.00f, 0.20f}, // c1
+            {0.00f, 0.80f}, // c2
         }};
 
-/*
-std::vector<Point> shapePoints =
+static BezierLine blinkShape[4] =
     {
-        {0.000000, 0.200000},
-        {0.058579, 0.058579},
-        {0.200000, 0.000000},
-        {0.800000, 0.000000},
-        {0.941421, 0.058579},
-        {1.000000, 0.200000},
-        {1.000000, 0.800000},
-        {0.941421, 0.941421},
-        {0.800000, 1.000000},
-        {0.200000, 1.000000},
-        {0.058579, 0.941421},
-        {0.000000, 0.800000}
-    };
-*/
+        // top
+        {
+            {-0.10f, 0.29f}, // p1
+            {1.10f, 0.29f},  // p2
+            {-0.00f, 0.24f}, // c1
+            {1.00f, 0.24f},  // c2
+        },
+        // right
+        {
+            {1.10f, 0.29f}, // p1
+            {1.10f, 0.20f}, // p2
+            {1.19f, 0.33f}, // c1
+            {1.20f, 0.20f}, // c2
+        },
+        // bottom
+        {
+            {1.10f, 0.20f},  // p1
+            {-0.10f, 0.20f}, // p2
+            {1.00f, 0.20f},  // c1
+            {0.00f, 0.20f},  // c2
+        },
+        // left
+        {
+            {-0.10f, 0.20f}, // p1
+            {-0.10f, 0.29f}, // p2
+            {-0.20f, 0.20f}, // c1
+            {-0.19f, 0.33f}, // c2
+        },
+};
+
+static BezierLine angryShape[4] =
+    {
+        // top
+        {
+            {0.10f, 0.65f}, // p1
+            {0.90f, 0.45f}, // p2
+            {0.20f, 0.60f}, // c1
+            {0.70f, 0.45f}, // c2
+        },
+        // right
+        {
+            {0.90f, 0.45f}, // p1
+            {0.90f, 0.25f}, // p2
+            {0.93f, 0.39f}, // c1
+            {0.93f, 0.30f}, // c2
+        },
+        // bottom
+        {
+            {0.90f, 0.25f}, // p1
+            {0.10f, 0.25f}, // p2
+            {0.85f, 0.15f}, // c1
+            {0.17f, 0.18f}, // c2
+        },
+        // left
+        {
+            {0.10f, 0.25f}, // p1
+            {0.10f, 0.65f}, // p2
+            {0.03f, 0.32f}, // c1
+            {0.05f, 0.55f}, // c2
+        },
+};
+
+static BezierLine happyShape[4] =
+    {
+        // top
+        {
+            {0.10f, 0.70f}, // p1
+            {0.90f, 0.70f}, // p2
+            {0.20f, 0.80f}, // c1
+            {0.80f, 0.80f}, // c2
+        },
+        // right
+        {
+            {0.90f, 0.70f}, // p1
+            {0.90f, 0.50f}, // p2
+            {0.95f, 0.65f}, // c1
+            {0.95f, 0.45f}, // c2
+        },
+        // bottom
+        {
+            {0.90f, 0.50f}, // p1
+            {0.10f, 0.50f}, // p2
+            {0.80f, 0.60f}, // c1
+            {0.20f, 0.60f}, // c2
+        },
+        // left
+        {
+            {0.10f, 0.50f}, // p1
+            {0.10f, 0.70f}, // p2
+            {0.05f, 0.45f}, // c1
+            {0.05f, 0.65f}, // c2
+        },
+};
+
+bool buttonUp = false;
+
+EyeRenderCache cacheL;
+EyeRenderCache cacheR;
+
+BezierLine shape[4];
 
 EyeState default_state =
     {
-        {0.0f, 0.0f},       //  gaze        -1 .. 1
-        {0.0, 0.0},         //  offset      -1 .. 1
-        {0.75f,0.75f},  //  size        0.1 .. 1.0
-        0.5f,               //  pupilSize   0.5 .. 1.2
-        0.0f,               //  blink       0..1
-        0.0f,               //  squash      0.8 .. 1.2
-        0.0f,               //  upperLid    0..1
-        0.0f,               //  lowerLid    0..1
-        {0, 255, 0},        //  color 
-        255
-    };
+        {0.0f, 0.0f},   //  gaze        -1 .. 1
+        {0.0, 0.0},     //  offset      -1 .. 1
+        {0.75f, 0.75f}, //  scale        0.1 .. 1.0
+        0.5f,           //  pupilSize   0.5 .. 1.2
+        0.0f,           //  blink       0..1
+        0.0f,           //  squash      0.8 .. 1.2
+        0.0f,           //  upperLid    0..1
+        0.0f,           //  lowerLid    0..1
+        0.0f,           //  agry
+        0.0f,           //  happy
+        {0, 255, 0},    //  color
+        255};
 
 EyeState start_state =
     {
-        {0.0f, 0.0f},       //  gaze        -1 .. 1
-        {0.0, 0.0},         //  offset      -1 .. 1
-        {1.0f, 0.1f},       //  size        0.1 .. 1.0
-        0.5f,               //  pupilSize   0.5 .. 1.2
-        0.0f,               //  blink       0..1
-        0.0f,               //  squash      0.8 .. 1.2
-        0.0f,               //  upperLid    0..1
-        0.0f,               //  lowerLid    0..1
-        {255, 0, 0},        //  color 
-        0
-    };
+        {0.0f, 0.0f}, //  gaze        -1 .. 1
+        {0.0, 0.0},   //  offset      -1 .. 1
+        {1.0f, 0.1f}, //  scale        0.1 .. 1.0
+        0.5f,         //  pupilSize   0.5 .. 1.2
+        0.0f,         //  blink       0..1
+        0.0f,         //  squash      0.8 .. 1.2
+        0.0f,         //  upperLid    0..1
+        0.0f,         //  lowerLid    0..1
+        0.0f,         //  agry
+        0.0f,         //  happy
+        {255, 0, 0},  //  color
+        0};
 
 EyePair eyePair;
 
@@ -169,59 +253,58 @@ EyeState eyeR;
 std::vector<Point> pts;
 
 lgfx::rgb888_t pupilGradient[] =
-{
-    {0, 0, 0},    // Zentrum
-    {0, 0, 0},    // Mitte
-    {0, 0, 0},     // Rand
-    {0, 0, 0},
-
+    {
+        {0, 0, 0}, // Zentrum
+        {0, 0, 0}, // Mitte
+        {0, 0, 0}, // Rand
+        {0, 0, 0},
 };
 
 lgfx::colors_t pupilColors =
-{
-    pupilGradient,
-    4
-};
+    {
+        pupilGradient,
+        4};
 
 int bezierRes = 5;
 
-Point joy = {0,0}; // to store the X-axis value
+Point joy = {0, 0}; // to store the X-axis value
 
 float blinkbefore;
-Point sizebefore;
+Point scalebefore;
 unsigned long animMillis = 0;
 
 const int PWM_CHANNEL = 0;
-const int PWM_FREQ = 5000;  // 5 kHz
-const int PWM_RESOLUTION = 8;  // 0-255
+const int PWM_FREQ = 5000;    // 5 kHz
+const int PWM_RESOLUTION = 8; // 0-255
 
 // Vector Eye
 void sampleBezier(const BezierLine &b, std::vector<Point> &pts, uint8_t steps);
 void buildShape(BezierLine *shape, int count, int steps, std::vector<Point> &pts);
-void normalizeToScreen(std::vector<Point> &pts, uint16_t w, uint16_t h, int16_t ox, int16_t oy);
-void fillPolygon(const std::vector<Point>& pts, LGFX_Sprite& eyeSpr, uint16_t color);
+void fillPolygon(const std::vector<Point> &pts, LGFX_Sprite &eyeSpr, uint16_t color);
 
 // Draw Funktions
-//void drawEye(LGFX_Sprite& eyeSpr, EyeState& e, uint16_t screen_x, uint16_t screen_y);
-void drawEye(LGFX_Sprite &eyeSpr, EyeState &e, EyeRenderCache& cache, uint16_t screen_x, uint16_t screen_y);
-void drawFace(EyePair& pair, EyeState& eL, EyeState& eR, int screen_x, int screen_y); // X ↕ Y ↔
+// void drawEye(LGFX_Sprite& eyeSpr, EyeState& e, uint16_t screen_x, uint16_t screen_y);
+void drawEye(LGFX_Sprite &eyeSpr, EyeState &e, EyeRenderCache &cache, uint16_t screen_x, uint16_t screen_y, bool mirror = false);
+void drawFace(EyePair &pair, EyeState &eL, EyeState &eR, int screen_x, int screen_y); // X ↕ Y ↔
 
-float lerp(float a, float b, float t = 0.1);
-Point lerp(Point a, Point b, float t = 0.1);
-uint8_t lerp(uint8_t a, uint8_t b, float t = 0.1);
-lgfx::rgb888_t lerpColor(const lgfx::rgb888_t& a, const lgfx::rgb888_t& b, float t);
+float lerp(float a, float b, float t = 0.1f);
+Point lerp(Point &a, Point &b, float t = 0.1f);
+Point lerp(const Point &a, const Point &b, float t = 0.1f);
+uint8_t lerp(uint8_t a, uint8_t b, float t = 0.1f);
+BezierLine lerp(const BezierLine &a, const BezierLine &b, float t = 0.1f);
+lgfx::rgb888_t lerpColor(const lgfx::rgb888_t &a, const lgfx::rgb888_t &b, float t = 0.1f);
 
-void buildGradient(lgfx::rgb888_t* grad, const lgfx::rgb888_t targetColor);
-bool updateColor(lgfx::rgb888_t& currentColor, lgfx::rgb888_t targetColor, float speed = 0.1f);
-void updateEyeState(EyeState& eye, EyeState& target, float speed = 0.1f);
+void buildGradient(lgfx::rgb888_t *grad, const lgfx::rgb888_t targetColor);
+bool updateColor(lgfx::rgb888_t &currentColor, lgfx::rgb888_t targetColor, float speed = 0.1f);
+void updateEyeState(EyeState &eye, EyeState &target, float speed = 0.1f);
 
-bool hasChanged(const Point& a, const Point& b, float eps = 0.001f);
-void updateShapeCache(EyeRenderCache& cache, EyeState& e);
-void buildEdgeTable(EyeRenderCache& cache);
-void fillPolygonET(EyeRenderCache& cache, LGFX_Sprite& spr, uint16_t color);
+bool hasChanged(const Point &a, const Point &b, float eps = 0.001f);
+void updateShapeCache(EyeRenderCache &cache, EyeState &e, bool mirror = false);
+void buildEdgeTable(EyeRenderCache &cache);
+void fillPolygonET(EyeRenderCache &cache, LGFX_Sprite &spr, uint16_t color);
 
-void applyLids(std::vector<Point>& pts, const EyeState& e);
-
+void deformShape(BezierLine *shape, const EyeState &e, bool mirror = false);
+void morphShape(BezierLine *out, const BezierLine *base, const BezierLine *target, float t = 0.1f);
 void blink(EyeState &e, EyeState &target, float blink);
 
 // FPS stuff
